55f46bd8567298319933c19d0c8328cecb09d71e
sruthy <sruthy@techversantinfo.com>
2021-07-18 14:10:59 +0530

    Removed pagination

--- AuthenticationApp/settings.py
+++ AuthenticationApp/settings.py
@@ -128,8 +128,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication'
